aff07a5fc831aeed7603341aa6e70a2cec239243
Sungbae Jeong <almagest0328@gmail.com>
2025-09-18 15:09:09 +0900

    foo

--- nob.c
+++ nob.c
@@ -6,13 +6,13 @@ int main(int argc, char** argv) {
 
     Nob_Cmd cmd = {0};
     nob_cmd_append(&cmd, "cc", "-std=gnu11", "-Wall", "-Wextra",
-        "-IC:/msys64/ucrt64/include", "-LC:/msys64/ucrt64/lib", "-lgsl", "-lgslcblas", "-lm",
+        "-lgsl", "-lgslcblas", "-lm",
         "-o", "main",
         "src/main.c",
         "src/bessel_func.c",
         "src/circle.c",
         "src/single_layer.c",
-        "src/sigle_circle_solv.c");
+        "src/single_circle_solv.c");
 
     if (!nob_cmd_run_sync(cmd)) return 1;
     return 0;
